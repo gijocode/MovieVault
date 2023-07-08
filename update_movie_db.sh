612315a45ce68f6b1d8402cc53462fa821b904d6
Gijo Varughese <75658977+gijocode@users.noreply.github.com>
2023-07-08 14:02:04 +0530

    update script

--- update_movie_db.sh
+++ update_movie_db.sh
@@ -1,6 +1,6 @@
 say -v Samantha "File added to the torrents folder. Running movie DB update"
 echo "File added to the folder. Running script...date is $(date)"
-source /Users/gijomathew/Code/MoviesWatchedShowcase/env/bin/activate
-python /Users/gijomathew/Code/MoviesWatchedShowcase/main.py
+source /Users/gijomathew/Code/MovieVault/env/bin/activate
+python /Users/gijomathew/Code/MovieVault/main.py
 deactivate
 say -v Samantha "DB updated!"
